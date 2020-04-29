21b9e47a26dd1e70cb1339dfc842c9823581766d
vsaleem <vic.saleem@gmail.com>
2020-04-28 23:31:58 -0400

    Began workin on schema.. created employee table.

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+CREATE TABLE employees(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id DECIMAL(10,0),
+    manager_id INT NULL,
+    PRIMARY KEY(id)
+);
