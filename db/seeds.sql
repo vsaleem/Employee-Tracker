3093084a9c65187a25eb4e1804999351f7ccbd76
vsaleem <vic.saleem@gmail.com>
2020-05-03 21:18:20 -0400

    Added INSERT INTO by employee_roles.

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,6 @@ USE employeetracker_db;
 INSERT INTO department(name)
 VALUES("Judge"), ("Attorney"), ("Caseworker"), ("CASA Volunteer");
 
+INSERT INTO employee_role(title, salary, department_id)
+VALUES('Judge', 89000, 1),('DCFS Supervisor', 49000, 2),('CASA Volunteer', 0, 3);
+
