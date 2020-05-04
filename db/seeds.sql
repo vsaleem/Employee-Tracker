bbb58498903970e2e03a15f679c2982b53cb635c
vsaleem <vic.saleem@gmail.com>
2020-05-03 21:23:21 -0400

    Added INSERT INTO by employees; need to rework names later

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,5 @@ VALUES("Judge"), ("Attorney"), ("Caseworker"), ("CASA Volunteer");
 INSERT INTO employee_role(title, salary, department_id)
 VALUES('Judge', 89000, 1),('DCFS Supervisor', 49000, 2),('CASA Volunteer', 0, 3);
 
+INSERT INTO employees(first_name, last_name, role_id, manager_id)
+VALUES("Beyonce", "K-Carter", 1, 1),("BlueIvy", "Carter", 2, 1),("Sarah", "Baskin", 3, 1),("Tiger", "King", 3, 1);
